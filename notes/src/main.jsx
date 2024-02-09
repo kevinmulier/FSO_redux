aaa941d60faf5085a25325030e2a9a31068edcd8
kevinmulier <kevin.mulier@outlook.fr>
2024-02-09 18:16:42 +0100

    improve note reducer

--- notes/src/main.jsx
+++ notes/src/main.jsx
@@ -1,23 +1,18 @@
 import ReactDOM from 'react-dom/client';
-import { combineReducers, createStore } from 'redux';
+import { combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
 
 import App from './App';
 import noteReducer, { createNote } from './reducers/noteReducer';
 import filterReducer, { filterChange } from './reducers/filterReducer';
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer,
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer,
+  },
 });
-
-const store = createStore(reducer);
-
-store.subscribe(() => console.log(store.getState()));
-store.dispatch(filterChange('IMPORTANT'));
-store.dispatch(
-  createNote('combineReducers forms one reducer from many simple reducers'),
-);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
